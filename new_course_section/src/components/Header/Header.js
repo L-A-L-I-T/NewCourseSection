7422d103ed5367c38c50522e8f87f2735a34679e
L-A-L-I-T <lrrajput2001@gmail.com>
2022-05-21 21:41:14 +0530

    minor changes

--- new_course_section/src/components/Header/Header.js
+++ new_course_section/src/components/Header/Header.js
@@ -1,13 +1,11 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
-import styles from "./Header.module.css";
 import BrandLogo from "../../assets/beyond-exams-logo.2c98e22c.svg";
 
 function header() {
 	return (
 		<div style={{ marginBottom: "100px" }}>
 			<AppBar
-				className={styles.appbar}
 				style={{
 					boxShadow: "0 0 10px 1px rgb(0 0 0 / 20%)",
 					backgroundColor: "#fff",
